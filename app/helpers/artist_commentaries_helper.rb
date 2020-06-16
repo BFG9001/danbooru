7165f2703bd3d07c2286ae969dbfd0ab696998a4
bfg9000 <bfg9000@jump.wtf>
2020-06-16 05:47:40 +0000

    Hid the donation/upgrade advertisement message as well as the promotional text. Also, added an auto-parser for 4chan.org/m/ post references in image descriptions (Artist Commentary) That will automatically link to the associated post in desuarchive.

--- app/helpers/artist_commentaries_helper.rb
+++ app/helpers/artist_commentaries_helper.rb
@@ -8,6 +8,17 @@ module ArtistCommentariesHelper
   end
 
   def format_commentary_description(description, classes: "")
+    _mSVdesuArchiveRefPattern = />>\d+\s/ #Whitespace at the end of expression to ensure already-linked post references don't get parsed
+    descriptionX = description + " " #account for an edge case where >>##### references at the EOF do not get counted because they lack a whitespace.
+    descriptionX.scan(_mSVdesuArchiveRefPattern).each do |postRef|
+      postRef = postRef[0..-2] #remove the whitespace from the regular expression. 
+      rawPostNumber = postRef[2..-1]
+      modifiedText = "\"" + postRef + "\"" ":[https://desuarchive.org/m/post/" + rawPostNumber + "]"
+      description.gsub!(postRef, modifiedText)
+      puts(description)
+    end
+    #The above is to automatically parse and hotlink ">>#######" post numbers from 4chan.org/m/ and turn them into links to desu archive. The actual linking is done by DText.
+
     tag.div(class: "prose #{classes}") do
       format_text(description, disable_mentions: true)
     end
---
+++ startup_normal.sh
@@ -0,0 +1,1 @@
+screen bundle exec rails server -e production --binding 127.0.0.1
